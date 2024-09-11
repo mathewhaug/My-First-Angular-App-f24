e3d37b87cb7f240f9ec435d4fcf7ac61d656cfd5
MHAUG <mhaug@stclaircollege.ca>
2024-09-11 14:52:40 -0400

    Showcasing commit for students

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {User} from "./models/user";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'My-First-Project';
+  title:string = 'Matt';
+  varName:string='Test'
+
+  user1: User = {id:1,firstName:'Matt',lastName:'Haug',department:'programming',isAdmin:false};
+  user2 : User = {id: 2, firstName: "Darren", lastName:"Takakki", department: "Web Dev", isAdmin: true};
+  //Can declare values either way
+  userList: User[] = //any[] would have worked as well
+    [this.user1,this.user2,
+      {id: 3, firstName: "John", lastName: "Doe", department:"Programming", isAdmin: false},
+      {id: 4, firstName: "Jane", lastName: "Doe", department:"Programming", isAdmin:true}
+    ]
+//test
+  //Create toggle functionality
+  toggleAdminStatus(user:User):void{
+    user.isAdmin = !user.isAdmin;
+  }
+
+
 }
