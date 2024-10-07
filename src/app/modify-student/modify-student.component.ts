e6016869ce32e32b7fcac83a491dc7efd53d450c
matt <mhaug@stclaircollege.ca>
2024-10-06 20:44:17 -0400

    Added comments

--- src/app/modify-student/modify-student.component.ts
+++ src/app/modify-student/modify-student.component.ts
@@ -42,19 +42,37 @@ This code initializes the component by fetching the details of a specific studen
   to view or modify the student's details.
  */
   ngOnInit(): void {
+    // first we retreive the student ID from the route parameters using the ActivatedRoute service
+    //the paramMap.get('id') method extracts the 'id' parameter from the route, and Number()
+    // converts it to a numeric value. This ID is then used to fetch the student's details.
     const id = Number(this.route.snapshot.paramMap.get('id'));
     if (id) {
+      //if the ID is valid, the StudentService is used to fetch the student's details by calling the getStudentById method
       this.studentService.getStudentById(id).subscribe(student => {
         if (student) {
+          //If the student object is valid, the patchValue method of the reactive form
+          // (studentForm) is called to populate the form with the student's data The patchValue method updates the form controls with the
+          // values from the student object without resetting the entire form
           this.studentForm.patchValue(student);
         }
       });
     }
   }
-
+/*
+onSubmit method in the ModifyStudentComponent class is responsible for handling
+ the form submission when the user attempts to save the students details.
+  This method first checks if the form is valid by using the valid property of the reactive form
+ */
   onSubmit(): void {
     if (this.studentForm.valid) {
+      //Iff the form is valid, it extracts the form values into a student object of type User
       const student: User = this.studentForm.value;
+      /*
+      Here we have a little bit of logic, first iff the student.id exists, we know that the student is already in the list
+      and just being updated
+
+      if it does not exist, we know that the student is new and we need to add it to the list
+       */
       if (student.id) {
         this.studentService.updateStudent(student).subscribe(() => this.router.navigate(['/students']));
       } else {
