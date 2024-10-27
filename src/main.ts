b4673cfb36142497cb0137b5e98b92380acb9bf9
matt <mhaug@stclaircollege.ca>
2024-10-27 17:05:01 -0400

    Lazy Loading Implemented.

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,32 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter, Routes } from '@angular/router';
-import { appConfig } from './app/app.config';
+
 import { AppComponent } from './app/app.component';
-import {StudentDetailComponent} from "./app/student-detail/student-detail.component";
-import {StudentListComponent} from "./app/student-list/student-list.component";
-import {ModifyStudentComponent} from "./app/modify-student/modify-student.component";
-import {PageNotFoundComponent} from "./app/page-not-found/page-not-found.component";
 import {HttpClientInMemoryWebApiModule} from "angular-in-memory-web-api";
 import {InMemoryDataService} from "./app/Services/in-memory-data.service";
-import {provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
+import {provideHttpClient} from "@angular/common/http";
 import {importProvidersFrom} from "@angular/core";
+import {StudentListComponent} from "./app/student-list/student-list.component";
 
+//main.ts
+//define the routes and specify their child routes
 const routes: Routes = [
-  {path:'', redirectTo: '/students', pathMatch: 'full'}, //default route
-  { path: 'students', component: StudentListComponent },
-  { path: 'students/:id', component: StudentDetailComponent },
-  {path:'modify-student', component: ModifyStudentComponent},
-  {path: '**', component:PageNotFoundComponent}//Wildcard route for a 404 page
+  { path: '', redirectTo: '/students', pathMatch: 'full' }, //Default route
+  { path: 'students', component: StudentListComponent },     //eagerly loaded
+  { path: 'students/:id',
+    loadComponent: () =>
+      import('./app/student-detail/student-detail.component').then(m => m.StudentDetailComponent) }, //Lazy Loaded
+  { path: 'modify-student',
+    loadComponent: () =>
+      import('./app/modify-student/modify-student.component').then(m => m.ModifyStudentComponent) },
+  { path: '**',
+    loadComponent: () =>
+      import('./app/page-not-found/page-not-found.component').then(m => m.PageNotFoundComponent) },
 ];
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(), // Ensure that HTTP interceptors are properly configured
     provideRouter(routes),
-    importProvidersFrom(HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { delay: 1000 })) // Import providers dynamically
+    importProvidersFrom(HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { delay: 1 })) // Import providers dynamically
   ],
 }).catch((err) => console.error(err));
